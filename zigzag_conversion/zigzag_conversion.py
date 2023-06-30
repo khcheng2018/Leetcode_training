7c6123c0e9a7048bc7d51b7d960d94542fe4b46a
CKH <38063292+khcheng2018@users.noreply.github.com>
2023-06-30 14:59:04 +0800

    Update zigzag_conversion.py

--- zigzag_conversion/zigzag_conversion.py
+++ zigzag_conversion/zigzag_conversion.py
@@ -10,6 +10,8 @@ class Solution(object):
         rows = 0
         list_all = [''] * numRows
         string = ''
+        if (numRows == 1):
+            return s
         for i in range (len_s):
             list_all[rows] = list_all[rows] + s[i]
             if (bool_direction):
