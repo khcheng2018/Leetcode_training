8d8f0cf71835b5ecaef03ee57519f26628f6264d
CKH <38063292+khcheng2018@users.noreply.github.com>
2023-06-30 14:55:18 +0800

    Create zigzag_conversion.py

---
+++ zigzag_conversion/zigzag_conversion.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        len_s = len(s)
+        bool_direction = True
+        rows = 0
+        list_all = [''] * numRows
+        string = ''
+        for i in range (len_s):
+            list_all[rows] = list_all[rows] + s[i]
+            if (bool_direction):
+                if (rows < (numRows-1)):
+                    rows = rows + 1
+                else: 
+                    rows = rows - 1
+                    bool_direction = False
+            else:
+                if (rows != 0):
+                    rows = rows - 1
+                else: 
+                    rows = rows + 1
+                    bool_direction = True
+        for j in range (numRows):
+            string = string + str(list_all[j])
+        return string
